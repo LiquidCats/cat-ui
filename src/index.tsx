530345208a54c41cd4620143ace3a003674e67fc
iShabanov <i.s.shabanov@ya.ru>
2021-05-22 21:21:37 +0300

    minor adjustments

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,55 @@ import {ThemeProvider} from "react-jss";
 
 import whiteTheme from "./theme.white";
 
-import reportWebVitals from './reportWebVitals';
+import Button from "./Button";
+import ButtonGroup from "./ButtonGroup";
 
 ReactDOM.render(
   <React.StrictMode>
       <ThemeProvider theme={whiteTheme}>
-          test
+          {/*<div>*/}
+          {/*    <h2>Weight</h2>*/}
+          {/*    <div><Button fixedWidth weight="small">small</Button></div><br/>*/}
+          {/*    <div><Button fixedWidth weight="default">default</Button></div><br/>*/}
+          {/*    <div><Button fixedWidth={180} weight="big">big with long text</Button></div><br/>*/}
+          {/*</div>*/}
+          {/*<div>*/}
+          {/*    <h2>Color</h2>*/}
+          {/*    <div><Button color="primary">primary</Button></div><br/>*/}
+          {/*    <div><Button color="default">default</Button></div><br/>*/}
+          {/*    <div><Button color="secondary">secondary</Button></div><br/>*/}
+          {/*</div>*/}
+          {/*<div>*/}
+          {/*    <h2>Variant</h2>*/}
+          {/*    <div></div><br/>*/}
+          {/*    <div></div><br/>*/}
+          {/*    <div></div><br/>*/}
+          {/*</div>*/}
+
+          {/*<div>*/}
+          {/*    <ButtonGroup>*/}
+          {/*        <Button color="primary" variant="default" disabled>test 3</Button>*/}
+          {/*        <Button color="default" variant="default" disabled>test 2</Button>*/}
+          {/*        <Button color="secondary" variant="default" disabled>test 1</Button>*/}
+          {/*    </ButtonGroup>*/}
+          {/*    <br/>*/}
+          {/*    <ButtonGroup>*/}
+          {/*        <Button color="primary" variant="outlined" disabled>test 3</Button>*/}
+          {/*        <Button color="default" variant="outlined" disabled>test 2</Button>*/}
+          {/*        <Button color="secondary" variant="outlined" disabled>test 1</Button>*/}
+          {/*    </ButtonGroup>*/}
+          {/*    <br/>*/}
+          {/*    <ButtonGroup>*/}
+          {/*        <Button color="primary" variant="text" disabled>test 3</Button>*/}
+          {/*        <Button color="default" variant="text" disabled>test 2</Button>*/}
+          {/*        <Button color="secondary" variant="text" disabled>test 1</Button>*/}
+          {/*    </ButtonGroup>*/}
+          {/*</div>*/}
+
+          <div>
+
+          </div>
       </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/theme.white.ts
+++ src/theme.white.ts
@@ -1,15 +1,43 @@
 const colors = {
     blue: '#3A5FD6',
-    gray: '#AAA9BC',
+    gray: '#adadb1',
     pink: '#FF606E',
-    black: '#464555',
+    black: '#53515e',
+    white: '#fff',
     red: '#CB1E3F',
 }
+const backgrounds = {
+    gray: 'rgba(158,158,165,0.2)',
+    blue: 'rgba(58, 95, 214, 0.2)',
+    red: 'rgba(203, 30, 63, 0.2)',
+    black: 'rgba(0, 0, 0, 0.2)',
+}
 
-const whiteTheme = {
-    fontSize: '16px',
-    fontColor: colors.black,
+export const whiteTheme = {
+    font: {
+        size: {
+            small: '0.8rem',
+            normal: '1rem',
+            big: '1.3rem',
+        },
+        color: colors.black,
+        family: "'Roboto', sans-serif",
+        weight: {
+            normal: 300,
+            bold: 500,
+            fat: 700,
+        },
+    },
     colors,
+    backgrounds,
+    border: {
+        radius: {
+            default: 5
+        }
+    },
+    transaction: ".3s ease-in-out"
 };
+
+export type UI = typeof whiteTheme;
 
 export default whiteTheme;
