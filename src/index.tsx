b1f8e401b14bae0dda24cbe46f5a778ccf82fb3d
iShabanov <i.s.shabanov@ya.ru>
2021-06-27 15:11:09 +0300

    apply grid

--- src/index.tsx
+++ src/index.tsx
@@ -7,52 +7,64 @@ import whiteTheme from "./theme.white";
 
 import Button from "./Button";
 import ButtonGroup from "./ButtonGroup";
+import Container from "./Container";
+import Grid from "./Grid";
 
 ReactDOM.render(
   <React.StrictMode>
       <ThemeProvider theme={whiteTheme}>
-          {/*<div>*/}
-          {/*    <h2>Weight</h2>*/}
-          {/*    <div><Button fixedWidth weight="small">small</Button></div><br/>*/}
-          {/*    <div><Button fixedWidth weight="default">default</Button></div><br/>*/}
-          {/*    <div><Button fixedWidth={180} weight="big">big with long text</Button></div><br/>*/}
-          {/*</div>*/}
-          {/*<div>*/}
-          {/*    <h2>Color</h2>*/}
-          {/*    <div><Button color="primary">primary</Button></div><br/>*/}
-          {/*    <div><Button color="default">default</Button></div><br/>*/}
-          {/*    <div><Button color="secondary">secondary</Button></div><br/>*/}
-          {/*</div>*/}
-          {/*<div>*/}
-          {/*    <h2>Variant</h2>*/}
-          {/*    <div></div><br/>*/}
-          {/*    <div></div><br/>*/}
-          {/*    <div></div><br/>*/}
-          {/*</div>*/}
+          <Container>
+              <Grid container>
+                  <Grid item>
+                      <h2>Weight</h2>
+                      <Button fixedWidth weight="small">small</Button>
+                      <Button fixedWidth weight="default">default</Button>
+                      <Button fixedWidth={180} weight="big">big with long text</Button>
+                  </Grid>
+                  <Grid item>
+                      <h2>Color</h2>
+                      <Button color="primary">primary</Button>
+                      <Button color="default">default</Button>
+                      <Button color="secondary">secondary</Button>
+                  </Grid>
+                  <Grid item>
+                      <h2>Variant</h2>
+                      <Button variant="outlined">outlined</Button>
+                      <Button variant="default">default</Button>
+                      <Button variant="text">text</Button>
+                  </Grid>
+              </Grid>
+              <Grid container>
+                  <Grid item>
+                      <ButtonGroup>
+                          <Button color="primary" variant="default" disabled>test 3</Button>
+                          <Button color="default" variant="default" disabled>test 2</Button>
+                          <Button color="secondary" variant="default" disabled>test 1</Button>
+                      </ButtonGroup>
+                  </Grid>
+                  <Grid item>
+                      <ButtonGroup>
+                          <Button color="primary" variant="outlined" disabled>test 3</Button>
+                          <Button color="default" variant="outlined" disabled>test 2</Button>
+                          <Button color="secondary" variant="outlined" disabled>test 1</Button>
+                      </ButtonGroup>
+                  </Grid>
+                  <Grid item>
+                      <ButtonGroup>
+                          <Button color="primary" variant="text" disabled>test 3</Button>
+                          <Button color="default" variant="text" disabled>test 2</Button>
+                          <Button color="secondary" variant="text" disabled>test 1</Button>
+                      </ButtonGroup>
+                  </Grid>
+              </Grid>
 
-          {/*<div>*/}
-          {/*    <ButtonGroup>*/}
-          {/*        <Button color="primary" variant="default" disabled>test 3</Button>*/}
-          {/*        <Button color="default" variant="default" disabled>test 2</Button>*/}
-          {/*        <Button color="secondary" variant="default" disabled>test 1</Button>*/}
-          {/*    </ButtonGroup>*/}
-          {/*    <br/>*/}
-          {/*    <ButtonGroup>*/}
-          {/*        <Button color="primary" variant="outlined" disabled>test 3</Button>*/}
-          {/*        <Button color="default" variant="outlined" disabled>test 2</Button>*/}
-          {/*        <Button color="secondary" variant="outlined" disabled>test 1</Button>*/}
-          {/*    </ButtonGroup>*/}
-          {/*    <br/>*/}
-          {/*    <ButtonGroup>*/}
-          {/*        <Button color="primary" variant="text" disabled>test 3</Button>*/}
-          {/*        <Button color="default" variant="text" disabled>test 2</Button>*/}
-          {/*        <Button color="secondary" variant="text" disabled>test 1</Button>*/}
-          {/*    </ButtonGroup>*/}
-          {/*</div>*/}
-
-          <div>
-
-          </div>
+              <Grid container>
+                  <Grid item xs={12}>test-1</Grid>
+                  <Grid item xs={12} md={6}>test-2</Grid>
+                  <Grid item xs={12} md={6}>test-3</Grid>
+                  <Grid item>test-4</Grid>
+              </Grid>
+          </Container>
       </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
