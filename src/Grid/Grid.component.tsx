e61757b414f47e4b1e3add3651d79b237fbee471
iShabanov <i.s.shabanov@ya.ru>
2021-07-03 00:17:18 +0300

    improve styles

--- src/Grid/Grid.component.tsx
+++ src/Grid/Grid.component.tsx
@@ -1,4 +1,4 @@
-import React, {forwardRef, HTMLAttributes} from "react";
+import React, {forwardRef, HTMLAttributes, memo} from "react";
 import {useTheme} from "react-jss";
 import clsx from "clsx";
 //
@@ -6,46 +6,52 @@ import {UI} from "../theme.white";
 import {GridProps} from "./Grid.types";
 import useStyles from "./Grid.styles";
 
-const Grid = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement> & GridProps>((props, ref) => {
-    const {
-        children,
-        className,
-        container = false,
-        item = false,
-        spacing = 2,
-        justify = 'center',
-        alignItems = 'flex-start',
-        alignContent = 'flex-start',
-        direction = 'row',
-        wrap = 'wrap',
-        xs  = false,
-        sm  = false,
-        md  = false,
-        lg  = false,
-        xl  = false,
-        xxl = false,
-        ...rest
-    } = props;
+const Grid = memo(
+    forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement> & GridProps>((props, ref) => {
+        const {
+            children,
+            className,
+            container = false,
+            item = false,
+            spacing = 2,
+            justify = 'center',
+            alignItems = 'flex-start',
+            alignContent = 'flex-start',
+            direction = 'row',
+            wrap = 'wrap',
+            xs  = false,
+            sm  = false,
+            md  = false,
+            lg  = false,
+            xl  = false,
+            xxl = false,
+            ...rest
+        } = props;
 
-    const theme = useTheme<UI>();
-    const styles = useStyles({theme, spacing, justify, alignItems, alignContent, direction, wrap, xs, sm, md, lg, xl, xxl, ...rest});
-    const classes = clsx(
-        styles.base,
-        container && styles.container,
-        item && styles.item,
-        (item && xs) && `xs-${xs}`,
-        (item && sm) && `sm-${sm}`,
-        (item && md) && `md-${md}`,
-        (item && lg) && `lg-${lg}`,
-        (item && xl) && `xl-${xl}`,
-        (item && xxl) && `xxl-${xxl}`,
-        className,
-    );
+        const theme = useTheme<UI>();
+        const styles = useStyles({theme, spacing, justify, alignItems, alignContent, direction, wrap, xs, sm, md, lg, xl, xxl, ...rest});
+        const classes = clsx(
+            styles.base,
+            container && styles.container,
+            item && styles.item,
+            (item && xs) && `xs-${xs}`,
+            (item && sm) && `sm-${sm}`,
+            (item && md) && `md-${md}`,
+            (item && lg) && `lg-${lg}`,
+            (item && xl) && `xl-${xl}`,
+            (item && xxl) && `xxl-${xxl}`,
+            className,
+        );
 
-    return <div className={classes}
-                ref={ref}
-                children={children}
-                {...rest}/>
-})
+        return <div className={classes}
+                    ref={ref}
+                    children={children}
+                    {...rest}/>
+    })
+)
+
+if (process.env.NODE_ENV !== 'production') {
+    Grid.displayName = 'Grid'
+}
 
 export default Grid
--- src/Grid/Grid.styles.ts
+++ src/Grid/Grid.styles.ts
@@ -2,8 +2,7 @@ import {createUseStyles} from "react-jss";
 import {UI} from "../theme.white";
 import {GridProps} from "./Grid.types";
 
-
-const useStyles = createUseStyles<'container'|'item'|'base', GridProps, UI>({
+const useStyles = createUseStyles<string, GridProps, UI>({
     base: {
         justifyContent: ({justify}) => justify,
         alignItems: ({alignItems}) => alignItems,
@@ -12,56 +11,43 @@ const useStyles = createUseStyles<'container'|'item'|'base', GridProps, UI>({
         flexWrap: ({wrap}) => wrap,
     },
     container: {
-        display: "flex",
-        flexWrap: "wrap",
-        boxSizing: "border-box",
+        display: 'flex',
+        flexWrap: 'wrap',
+        boxSizing: 'border-box',
         width: ({theme, spacing = 2}) => `calc(100% + ${theme.grid.padding * spacing}px * 2)`,
         margin: ({theme, spacing= 2}) => [-(theme.grid.padding * spacing)],
     },
     item: ({theme, spacing = 4}) => {
-        const basicStyles = {
-            margin: 0,
-            flexGrow: 1,
-            boxSizing: "border-box",
-            padding: theme.grid.padding * spacing,
-        }
         const breakpoints = theme?.grid?.breakpoints?.keys || [];
         const breakpointValues: any = theme?.grid?.breakpoints?.values || [];
         const sizes = generateSizes(theme?.grid.columns);
         const columns = theme?.grid?.columns || 12;
-
         return {
-            ...basicStyles,
+            margin: 0,
+            flexGrow: 1,
+            boxSizing: 'border-box',
+            padding: theme.grid.padding * spacing,
             ...breakpoints.reduce<{[k: string]: any}>((globalStyles, breakpoint) => {
-                return sizes.reduce((globalStyles, size) => {
-                    let styles;
-
+                return sizes.reduce((globalStyles, size: any) => {
                     const name = `&.${breakpoint}-${size}`;
                     const value = breakpointValues[breakpoint];
-                    if (!globalStyles[name]) {
-                        globalStyles[name] = {}
-                    }
+                    const width = percentage(size, columns);
+                    const media = mediaUp(value);
 
-                    if (size === true) {
-                        styles = {flexGrow: 1, maxWidth: '100%', flexBasis: 0};
-                    } else if (size === 'auto') {
-                        styles = {flexGrow: 0, maxWidth: 'none', flexBasis: 'auto'};
-                    } else {
-                        // @ts-ignore
-                        const width = percentage(size, columns);
-                        styles = {flexGrow: 0, maxWidth: width, flexBasis: width};
-                    }
+                    if (!globalStyles[name]) globalStyles[name] = {}
 
-                    if (value === 0) {
-                        globalStyles[name] = styles;
-                    } else {
-                        globalStyles[name][mediaUp(value)] = styles
-                    }
+                    const styles = (size === true)
+                        ? {flexGrow: 1, maxWidth: '100%', flexBasis: 0}
+                        : (size === 'auto')
+                            ? {flexGrow: 0, maxWidth: 'none', flexBasis: 'auto'}
+                            : {flexGrow: 0, maxWidth: width, flexBasis: width}
+
+                    0 === value ? globalStyles[name] = styles : globalStyles[name][media] = styles
 
                     return globalStyles
                 }, globalStyles);
             }, {})
-        };
+        }
     },
 }, {name: 'grid'})
 
