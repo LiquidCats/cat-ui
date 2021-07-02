303dd63b84f2a3a5f9c574fcc1dd05e801f16912
iShabanov <i.s.shabanov@ya.ru>
2021-07-03 00:15:05 +0300

    improve styles

--- src/ButtonGroup/ButtonGroup.component.tsx
+++ src/ButtonGroup/ButtonGroup.component.tsx
@@ -1,4 +1,4 @@
-import React, {forwardRef, HTMLAttributes, MouseEvent} from "react";
+import React, {forwardRef, HTMLAttributes, memo, MouseEvent} from "react";
 import clsx from "clsx";
 import {useTheme} from "react-jss";
 //
@@ -7,21 +7,27 @@ import useStyles from './ButtonGroup.styles';
 import {ButtonGroupProps} from './ButtonGroup.types';
 import ButtonGroupContext from "./ButtonGroup.context";
 
-const ButtonGroup = forwardRef<HTMLDivElement, HTMLAttributes<ButtonGroupProps>>((props, ref) => {
-    const {
-        className,
-        children,
-        onClick = (e: MouseEvent) => {},
-        ...rest
-    } = props
-    const theme = useTheme<UI>();
-    const cls = useStyles({theme});
-    const classes = clsx(cls.group, className);
-    return <div {...rest} className={classes} ref={ref}>
-        <ButtonGroupContext.Provider value={{onClick, grouped: true}}>
-            {children}
-        </ButtonGroupContext.Provider>
-    </div>
-})
+const ButtonGroup = memo(
+    forwardRef<HTMLDivElement, HTMLAttributes<ButtonGroupProps>>((props, ref) => {
+        const {
+            className,
+            children,
+            onClick = (e: MouseEvent) => {},
+            ...rest
+        } = props
+        const theme = useTheme<UI>();
+        const cls = useStyles({theme});
+        const classes = clsx(cls.group, className);
+        return <div {...rest} className={classes} ref={ref}>
+            <ButtonGroupContext.Provider value={{onClick, grouped: true}}>
+                {children}
+            </ButtonGroupContext.Provider>
+        </div>
+    })
+)
+
+if (process.env.NODE_ENV !== 'production') {
+    ButtonGroup.displayName = 'ButtonGroup'
+}
 
 export default ButtonGroup;
